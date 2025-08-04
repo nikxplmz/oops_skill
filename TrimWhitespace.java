a3f940160fb410e87f8c31a8e71e87dbfb0d06a9
Nikhil Valluri <nikhilvalluri123@gmail.com>
2025-08-04 08:56:18 +0530

    Add files via upload

---
+++ TrimWhitespace.java
@@ -0,0 +1,10 @@
+package skill;
+
+public class TrimWhitespace {
+	class TrimWhitespace {
+	    public static void main(String[] args) {
+	        String s = "  Hello  ";
+	        System.out.println("[" + s.trim() + "]");     
+	    }
+
+}
