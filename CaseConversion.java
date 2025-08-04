722965f6005cb75f4fc17caf9b08628b31b245d6
Nikhil Valluri <nikhilvalluri123@gmail.com>
2025-08-04 08:51:41 +0530

    Add files via upload

---
+++ CaseConversion.java
@@ -0,0 +1,11 @@
+package skill;
+
+public class CaseConversion {
+
+	 public static void main(String[] args) {
+	        String s = "hello world";
+	        System.out.println(s.toUpperCase());   
+	        System.out.println(s.toLowerCase());   
+	    }
+	}
+
