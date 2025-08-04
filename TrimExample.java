057844858cb66891a74305f0e1b25b94b46b40f1
Nikhil Valluri <nikhilvalluri123@gmail.com>
2025-08-04 08:56:03 +0530

    Add files via upload

---
+++ TrimExample.java
@@ -0,0 +1,10 @@
+package skill;
+
+public class TrimExample {
+	public static void main(String[] args) {
+        String raw = "   Welcome to Java!   ";
+        System.out.println("Before Trim: [" + raw + "]");
+        System.out.println("After Trim: [" + raw.trim() + "]");
+    }
+
+}
