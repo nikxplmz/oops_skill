64e80e958d6803056722fd4a69f9954a65e76110
Nikhil Valluri <nikhilvalluri123@gmail.com>
2025-08-04 08:53:56 +0530

    Add files via upload

---
+++ SplitExample.java
@@ -0,0 +1,11 @@
+package skill;
+
+public class SplitExample {
+    public static void main(String[] args) {
+        String s = "apple,banana,grape,orange";
+        String[] fruits = s.split(",");
+        for(String fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
+}
