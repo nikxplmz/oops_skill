2e2028c4d4fff4f2a03d5718dd58b7e456f57955
Nikhil Valluri <nikhilvalluri123@gmail.com>
2025-08-04 08:54:55 +0530

    Add files via upload

---
+++ StringJoinExample.java
@@ -0,0 +1,12 @@
+package skill;
+
+public class StringJoinExample {
+	public static void main(String[] args) {
+        String a = "Hello";
+        String b = "World";
+        System.out.println(a.concat(" ").concat(b));  // Hello World
+        String joined = String.join("-", "A", "B", "C");
+        System.out.println(joined);  // A-B-C
+    }
+
+}
