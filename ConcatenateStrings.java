e6c7bfcef8b8d55ef01681789913ca1a870a102b
Nikhil Valluri <nikhilvalluri123@gmail.com>
2025-08-04 08:52:12 +0530

    Add files via upload

---
+++ ConcatenateStrings.java
@@ -0,0 +1,10 @@
+package skill;
+
+public class ConcatenateStrings {
+	 public static void main(String[] args) {
+	        String first = "Hello";
+	        String second = "World";
+	        String result = first + " " + second;
+	        System.out.println("Concatenated: " + result);
+	    }
+}
