0164a6ccfb916de61a7c2a7291a31e6f8699ac3b
Nikhil Valluri <nikhilvalluri123@gmail.com>
2025-08-04 08:53:36 +0530

    Add files via upload

---
+++ ReplaceModify.java
@@ -0,0 +1,12 @@
+package skill;
+
+public class ReplaceModify {
+	public static void main(String[] args) {
+    String s = "ababab";
+    System.out.println(s.replace('a', 'x'));         
+    System.out.println(s.replace("ab", "xy"));     
+    System.out.println(s.replaceAll("a.", "z"));    
+    System.out.println(s.replaceFirst("ab", "xy")); 
+}
+
+}
