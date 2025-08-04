a5ed2645ee8b7aa7a2fc8febed3f84cb80002eec
Nikhil Valluri <nikhilvalluri123@gmail.com>
2025-08-04 08:51:13 +0530

    Add files via upload

---
+++ substringexample1.java
@@ -0,0 +1,9 @@
+package skill;
+
+public class substringexample1 {
+	public static void main(String[] args) {
+        String s1 = "JavaProgramming";
+        String sub = s1.substring(4, 11); // "Program"
+        System.out.println("Substring: " + sub);
+    }
+}
